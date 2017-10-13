8c6120a91f156b4c4db717976c24e3269f63cbe0
Alexsey Zazvonnykh <kop3nha@gmail.com>
2017-10-13 21:15:12 +0300

    Переделана многопоточность, для balls.h скрыта реализация, всё перенесено на glfw

---
+++ Balls/balls.cpp
@@ -0,0 +1,380 @@
+#pragma once
+
+#include "balls.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+using namespace BaluLib;
+
+struct TThreadInfo
+{
+	std::atomic_flag processing;
+	volatile bool finished = false;
+
+	bool is_main_thread;
+	int offset, high;
+};
+
+TThreadInfo threads[threads_count];
+
+unsigned char grid_count[blocks_count*blocks_count];
+
+//static std::atomic_flag grid_lock[blocks_count*blocks_count];
+
+int grid[blocks_count*blocks_count*MAX_BALLS_IN_BLOCK];
+TVec2_Float balls_pos[balls_count];
+TVec2_Float balls_speed[balls_count];
+
+TVec<unsigned char, 4> balls_color[balls_count];
+
+int action = 0;
+TVec2 mouse_world_pos;
+float attractor_size = 30;
+
+void SetAttractorPos(TVec2 attractor_pos)
+{
+	mouse_world_pos = attractor_pos;
+}
+
+void SetAction(int value)
+{
+	action = value;
+}
+
+void ChangeAttractor(double offset)
+{
+	attractor_size += offset;
+	Clamp(0.0f, room_size / 2.0f, attractor_size);
+	attractor_size = Clamp(0.01f, 1000.0f, attractor_size);
+}
+
+TVec2_Float* GetBallsPosArray()
+{
+	return balls_pos;
+}
+
+TVec<unsigned char, 4>* GetBallsColorArray()
+{
+	return balls_color;
+}
+
+inline void Collide(int p1, int p2)
+{
+	TVec2_Float  delta = balls_pos[p2] - balls_pos[p1];
+	TFloat quadlen = delta.SqrLength();
+	if (quadlen < TFloat(sqr(ball_rad*2.0f)))
+	{
+		TFloat diff = (quadlen - TFloat(sqr(ball_rad*2.0f)));
+		TVec2_Float delta_speed = balls_speed[p2] - balls_speed[p1];
+
+		delta_speed >>= 3;
+		diff <<= 6;
+		TVec2_Float force = delta*diff + delta_speed;
+
+		balls_speed[p1] += force;
+		balls_speed[p2] -= force;
+	}
+}
+inline void CollideWithCell(int ball, int cell)
+{
+	//while (grid_lock[cell].test_and_set(std::memory_order_acquire));
+	if (MAX_BALLS_IN_BLOCK>2)
+	{
+		for (int s = 0; s<grid_count[cell]; s++)
+		{
+			int other_ball = grid[cell*MAX_BALLS_IN_BLOCK + s];
+			if (other_ball != ball)
+				Collide(ball, other_ball);
+		}
+	}
+	else
+	{
+		int count = grid_count[cell];
+		if (count >= 1)
+		{
+			int other_ball = grid[cell*MAX_BALLS_IN_BLOCK];
+			if (other_ball != ball)
+				Collide(ball, other_ball);
+			if (count >= 2)
+			{
+				other_ball = grid[cell*MAX_BALLS_IN_BLOCK + 1];
+				if (other_ball != ball)
+					Collide(ball, other_ball);
+			}
+		}
+	}
+	//grid_lock[cell].clear(std::memory_order_release);
+}
+
+inline void FindBallIndex(int start_block, int index, int& block, int& block_offset)
+{
+	int cell_id = int(balls_pos[index][1])*blocks_count + int(balls_pos[index][0]);
+	block = cell_id;
+
+	if (MAX_BALLS_IN_BLOCK>2)
+	{
+		for (int s = 0; s<grid_count[cell_id]; s++)
+		{
+			int b = grid[cell_id*MAX_BALLS_IN_BLOCK + s];
+			if (b == index)
+			{
+				block_offset = s;
+				return;
+			}
+		}
+	}
+	else
+	{
+		int count = grid_count[cell_id];
+		if (count >= 1)
+		{
+			if (grid[cell_id*MAX_BALLS_IN_BLOCK] == index)
+			{
+				block_offset = 0;
+				return;
+			}
+			if (count >= 2)
+			{
+				if (grid[cell_id*MAX_BALLS_IN_BLOCK + 1] == index)
+				{
+					block_offset = 1;
+					return;
+				}
+			}
+		}
+	}
+}
+
+void RedistrBalls()
+{
+	int last_id = -1;
+	for (int i = 0; i<(blocks_count*blocks_count); i++)
+	{
+		for (int s = 0; s<grid_count[i]; s++)
+		{
+			int b = grid[i*MAX_BALLS_IN_BLOCK + s];
+			if (b == last_id + 1)last_id = b;
+			else
+			{
+				int block = -1;
+				int block_offset = -1;
+				while (block_offset == -1)
+				{
+					last_id++;
+					assert(last_id<balls_count);
+					FindBallIndex(i, last_id, block, block_offset);
+				}
+				int other_ball = last_id;
+
+				assert(other_ball<balls_count&&other_ball >= 0);
+				assert(b<balls_count&&b >= 0);
+				Swap(balls_color[b], balls_color[other_ball]);
+				Swap(balls_pos[b], balls_pos[other_ball]);
+				Swap(balls_speed[b], balls_speed[other_ball]);
+
+				int g1 = i*MAX_BALLS_IN_BLOCK + s;
+				int g2 = block*MAX_BALLS_IN_BLOCK + block_offset;
+				assert(g1 >= 0 && g1<blocks_count*blocks_count*MAX_BALLS_IN_BLOCK);
+				assert(g2 >= 0 && g2<blocks_count*blocks_count*MAX_BALLS_IN_BLOCK);
+				Swap(grid[g1], grid[g2]);
+			}
+		}
+	}
+}
+
+int BroadPhase(void* p)
+{
+	TThreadInfo* params = (TThreadInfo*)p;
+	do
+	{
+		if (!params->is_main_thread)
+			while (params->processing.test_and_set(std::memory_order_acquire))
+			{
+				thrd_yield();
+			}
+
+		for (int i = params->offset; i <= params->high; i++)
+		{
+			int cell_x = balls_pos[i][0] + TFloat(0.5f);
+			int cell_y = balls_pos[i][1] + TFloat(0.5f);
+			int cell_id = cell_y*blocks_count + cell_x;
+
+			if (cell_y>0)
+			{
+				int temp = cell_id - blocks_count;
+				if (cell_x>0)CollideWithCell(i, temp - 1);
+				CollideWithCell(i, temp);
+			}
+			if (cell_x>0)
+				CollideWithCell(i, cell_id - 1);
+			CollideWithCell(i, cell_id);
+		}
+		if (!params->is_main_thread)
+		{
+			params->finished = true;
+		}
+	} while (!params->is_main_thread);
+
+	return 0;
+}
+
+void InitGrid()
+{
+	memset(&grid_count, 0, sizeof(grid_count));
+	for (int i = 0; i<balls_count; i++)
+	{
+		int cell_id = int(balls_pos[i][0]) + int(balls_pos[i][1])*blocks_count;
+		if (grid_count[cell_id]<MAX_BALLS_IN_BLOCK)
+		{
+			grid[cell_id*MAX_BALLS_IN_BLOCK + (grid_count[cell_id]++)] = i;
+		}
+	}
+}
+
+void UpdateBalls(bool move)
+{
+	TVec2_Float attractor_pos(mouse_world_pos[0], mouse_world_pos[1]);
+	if (move)
+	{
+		for (int i = 0; i<balls_count; i++)
+		{
+			balls_speed[i][1] += TFloat(-gravity);
+			if (action != 0)
+			{
+				TVec2_Float attractor = attractor_pos - balls_pos[i];
+				if (IsIn(attractor[0], TFloat(-attractor_size), TFloat(attractor_size))
+					&& IsIn(attractor[1], TFloat(-attractor_size), TFloat(attractor_size)))
+				{
+					if (true)
+					{
+						attractor >>= 4;
+						attractor *= action;
+						balls_speed[i] += attractor;
+					}
+					else
+					{
+						TFloat temp = attractor.SqrLength();
+						if (temp<TFloat((float)sqr(attractor_size)) && temp>TFloat(0.0f))
+						{
+							balls_speed[i] += attractor / TFloat((float)sqrt(temp))*TFloat(action*3.0f);
+
+						}
+					}
+				}
+			}
+			balls_speed[i] -= balls_speed[i] >> 11;
+
+			balls_pos[i] += balls_speed[i] >> 8;
+			if (balls_pos[i][0]>TFloat(room_size - ball_rad)) {
+				balls_pos[i][0] = room_size - ball_rad - 0.01f;
+				balls_speed[i][0] = 0;
+			}
+			else if (balls_pos[i][0]<TFloat(ball_rad)) {
+				balls_pos[i][0] = ball_rad + 0.01f;
+				balls_speed[i][0] = 0;
+			}
+			if (balls_pos[i][1]>TFloat(room_size - ball_rad)) {
+				balls_pos[i][1] = room_size - ball_rad - 0.01f;
+				balls_speed[i][1] = 0;
+			}
+			else if (balls_pos[i][1]<TFloat(ball_rad)) {
+				balls_pos[i][1] = ball_rad + 0.01f;
+				balls_speed[i][1] = 0;
+			}
+		}
+	}
+	InitGrid();
+
+	//start all processing threads
+	for (int i = 0; i < threads_count - 1; i++)
+		threads[i + 1].processing.clear(std::memory_order_release);
+
+	BroadPhase(&threads[0]);
+
+	bool finished = false;
+	do
+	{
+		finished = true;
+		for (int i = 0; i < threads_count - 1; i++)
+			finished &= threads[i + 1].finished;
+	} while (!finished);
+
+	for (int i = 0; i < threads_count - 1; i++)
+		threads[i + 1].finished = false;
+
+	//перераспределяем шары для более эффективного кэширования
+	if (true)
+	{
+		static unsigned int phys_frame = 0;
+		phys_frame++;
+		if (phys_frame % 7 == 0)
+			RedistrBalls();
+	}
+}
+
+void SendBallsPos(TVec<short, 2>* points)
+{
+	for (int i = 0; i<balls_count; i++)
+		points[i] = TVec<short, 2>(
+			balls_pos[i][0].GerRAW() >> (fracture_part - ball_pos_draw_precision),
+			balls_pos[i][1].GerRAW() >> (fracture_part - ball_pos_draw_precision));
+}
+
+void InitThreads()
+{
+	threads[0].offset = 0;
+	threads[0].is_main_thread = true;
+	threads[0].high = threads[0].offset + balls_on_thread - 1;
+	for (int i = 1; i<threads_count; i++)
+	{
+		threads[i].is_main_thread = false;
+		threads[i].finished = false;
+		threads[i].offset = i*balls_on_thread;
+		if (i == threads_count - 1)
+			threads[i].high = balls_count - 1;
+		else
+			threads[i].high = threads[i].offset + balls_on_thread - 1;
+
+		thrd_t physics_thread;
+		thrd_create(&physics_thread, BroadPhase, &threads[i]);
+	}
+}
+
+void InitBalls()
+{
+	InitThreads();
+
+	for (int i = 0; i<balls_count; i++)
+	{
+		int balls_on_line = room_size / (ball_rad * 2 + 0.01);
+		if (false)
+		{
+			balls_pos[i] =
+				TVec2_Float(
+				(short int)(i%balls_on_line),
+					(short int)(i / balls_on_line)) + TVec2_Float(0.5f, 0.5f)*(1 + Randfs()*0.001f);
+		}
+		else
+		{
+			balls_pos[i] = TVec2_Float(0.5f + Randf()*(room_size - 1), 0.5f + Randf()*(room_size - 1));
+		}
+	}
+
+	for (int i = 0; i<balls_count; i++)
+	{
+		balls_color[i] = TVec<unsigned char, 4>(
+			255 * sinf(balls_pos[i][1] * TFloat(70.0f / room_size)),
+			int(balls_pos[i][0] * TFloat(255.0f / room_size)),
+			int(balls_pos[i][1] * TFloat(255.0f / room_size)),
+			255);
+	}
+}
+
+void CopyPosAndColor(TVec2_Float *pos, TVec<unsigned char, 4> *color)
+{
+	memcpy(pos, balls_pos, sizeof(balls_pos));
+	memcpy(color, balls_color, sizeof(balls_color));
+}
--- Balls/main.cpp
+++ Balls/main.cpp
@@ -1,23 +1,17 @@
-#if defined(_MSC_VER)
-// Make MS math.h define M_PI
-#define _USE_MATH_DEFINES
-#endif
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <time.h>
-
-extern "C" {
-#include "../glfw/deps/tinycthread.h"
-}
+#include <atomic>
 
 #include <GLFW/glfw3.h>
-
+#include <IL/ilut.h>
 #include "baluRender.h"
-
 #include "balls.h"
+
+std::atomic_flag balls_draw_lock = ATOMIC_FLAG_INIT;
 
 #if defined(WIN32)||defined(_WIN32)
 #else
@@ -25,230 +19,133 @@ extern "C" {
 #endif
 
 using namespace BaluRender;
-
 TBaluRender* render;
 
-
-// Window dimensions
-float aspect_ratio;
-
-// Thread synchronization
-struct {
-	double    t;         // Time (s)
-	float     dt;        // Time since last frame (s)
-	cnd_t     p_done;    // Condition: particle physics done
-	cnd_t     d_done;    // Condition: particle draw done
-	mtx_t     particles_lock; // Particles data sharing mutex
-} thread_sync;
-
 TVec2 cursor_pos;
-
 bool balls_pause = false;
 bool test_broadphase = false;
 
-TTime balu_time;
+TTime draw_fps, phys_fps;
 
 using namespace TBaluRenderEnums;
-
 TVertexBufferId pos_buff;
-
 #ifdef USE_COLOR
 TVertexBufferId color_buff;
 #endif
-
 TMatrix<float, 4> ortho_m, ortho_m_inv;
 
-double draw_time;
+TVec2_Float local_balls_pos[balls_count];
+TVec<unsigned char, 4> local_balls_color[balls_count];
 
 TStreamsDesc streams;
 
+int phys_fps_value;
+double phys_time;
+
+bool volatile should_close = false;
+bool volatile draw_waiting = false;
+
 void Init()
 {
-
-	{
-		threads[0].offset = 0;
-		threads[0].is_main_thread = true;
-		threads[0].high = threads[0].offset + balls_on_thread - 1;
-
-		//for (int i = 1; i<threads_count; i++)
-		//{
-		//	threads[i].broadphase_event = CreateEvent(NULL, TRUE, false, NULL);
-		//	threads[i].end_broadphase_event = CreateEvent(NULL, TRUE, true, NULL);
-		//	threads[i].is_main_thread = false;
-		//	threads[i].offset = i*balls_on_thread;
-		//	threads[i].high = threads[i].offset + balls_on_thread - 1;
-		//	_beginthread(BroadPhase, 0, &threads[i]);
-		//}
-	}
+	
+	InitBalls();
 
 	render->Set.ClearColor(0, 0, 0);
 	float f = powf(2, fracture_part);
-	ortho_m = TMatrix<float, 4>::GetOrtho(TVec2(1, 1)*room_size*0.5*f , TVec2(1, 1)*room_size*1.1*f , -10, 10);
+	ortho_m = TMatrix<float, 4>::GetOrtho(TVec2(1, 1)*room_size*0.5*f, TVec2(1, 1)*room_size*1.1*f, -10, 10);
 	ortho_m_inv = ortho_m.GetInverted();
-
 	render->Set.Projection(ortho_m);
 	render->Set.Color(1, 1, 1, 1);
-	//render->Blend.Enable(true);
-	//render->Blend.Func("dc*(1-sa)+sc*sa");
 
-	//render->Set.PointSize(point_size);
-	//render->Set.PointSmooth(true);
-
-	//sprintf_s(render->log_buff, " passed\n"); render->log_file.Write(render->log_buff);
-
-	//tex = render->Texture.Create("frame_3.png");
-
-	for (int i = 0; i<balls_count; i++)
-	{
-		int balls_on_line = room_size / (ball_rad * 2 + 0.01);
-		if (false)
-		{
-			balls_pos[i] = 
-				TVec2_Float(
-				(short int)(i%balls_on_line),
-				(short int)(i / balls_on_line)) + TVec2_Float(0.5f, 0.5f)*(1 + Randfs()*0.001f);
-		}
-		else
-		{
-			balls_pos[i] = TVec2_Float(0.5f + Randf()*(room_size - 1), 0.5f + Randf()*(room_size - 1));
-		}
-	}
-
-	//sprintf_s(render->log_buff, "VBuffers creation..."); render->log_file.Write(render->log_buff);
-	pos_buff = render->VertexBuffer.Create(TVBType::Array, balls_count*sizeof(TVec<short, 2>), TVBRefresh::Stream, TVBUsage::Draw);
+	pos_buff = render->VertexBuffer.Create(TVBType::Array, balls_count * sizeof(TVec<short, 2>), TVBRefresh::Stream, TVBUsage::Draw);
 	TVec<short, 2>* points = (TVec<short, 2>*)render->VertexBuffer.Map(pos_buff, TVBAccess::Write);
 	SendBallsPos(points);
 	render->VertexBuffer.Unmap(pos_buff);
 
 #ifdef USE_COLOR
-	color_buff = render->VertexBuffer.Create(TVBType::Array, balls_count*sizeof(TVec<unsigned char, 4>), TVBRefresh::Static, TVBUsage::Draw);
+	color_buff = render->VertexBuffer.Create(TVBType::Array, balls_count * sizeof(TVec<unsigned char, 4>), TVBRefresh::Static, TVBUsage::Draw);
 	TVec<unsigned char, 4>* colors = (TVec<unsigned char, 4>*)render->VertexBuffer.Map(color_buff, TVBAccess::Write);
-
 	for (int i = 0; i<balls_count; i++)
 	{
-		balls_color[i] = TVec<unsigned char, 4>(
-			255 * sinf(balls_pos[i][1] * TFloat(70.0f / room_size)),
-			int(balls_pos[i][0] * TFloat(255.0f / room_size)),
-			int(balls_pos[i][1] * TFloat(255.0f / room_size)),
-			255);
-		colors[i] = balls_color[i];
+		colors[i] = GetBallsColorArray()[i];
 	}
 	render->VertexBuffer.Unmap(color_buff);
 #endif
-	//sprintf_s(render->log_buff, " passed\n"); render->log_file.Write(render->log_buff);
-
 
 #ifdef USE_COLOR
 	{
 		TVec<unsigned char, 4>* colors = (TVec<unsigned char, 4>*)render->VertexBuffer.Map(color_buff, TVBAccess::Write);
-		memcpy(colors, &balls_color, sizeof(balls_color));
+		memcpy(colors, GetBallsColorArray(), sizeof(TVec<unsigned char, 4>)* balls_count);
 		render->VertexBuffer.Unmap(color_buff);
 	}
-
 	streams.AddStream(TStream::Color, TDataType::UByte, 4, color_buff);
 #endif
-
-	render->VertexBuffer.Data(pos_buff, balls_count*sizeof(TVec<TFloat, 2>), &balls_pos[0]);
+	render->VertexBuffer.Data(pos_buff, balls_count * sizeof(TVec<TFloat, 2>), GetBallsPosArray());
 	streams.AddStream(TStream::Vertex, TDataType::Int, 2, pos_buff);
+
+	
 }
 
-static void draw_scene(GLFWwindow* window, double tt)
+void error_callback(int error, const char* description)
 {
+	puts(description);
+}
 
-	//if (balu_time.GetDelta()<0.001)return;
-	//balu_time.Tick();
+static bool draw_scene(GLFWwindow* window, double tt)
+{
+	static double t_old = 0.0;
+	float dt;
 
-	//action = KeyDown(VK_LBUTTON) ? 1 : (KeyDown(VK_RBUTTON) ? -1 : 0);
-	//{
-	//	TVec2i temp = GetCursorPos();
-	//	TVec2 tt = render->ScreenToClipSpace(temp[0], temp[1]);
-	//	TVec3 mm = (ortho_m_inv*(TVec4(TVec3(tt, 0), 1))).GetHomogen();
-	//	float f = powf(2, fracture_part);
-	//	mouse_world_pos[0] = mm[0] / f;
-	//	mouse_world_pos[1] = mm[1] / f;
-	//}
-	//if (!balls_pause)
-	//{
-		//UpdateBalls(balu_time, !test_broadphase);
-	//}
+	double t = glfwGetTime();
 
-	auto t = balu_time.GetTime();
-	//balu_time.Tick();
-	if (balu_time.ShowFPS())
+	dt = (float)(t - t_old);
+
+	if (dt < 0.01)
+		return false;
+	else
+		t_old = t;
+
+	draw_fps.Tick();
+	if (draw_fps.ShowFPS())
 	{
 		char buf[1000];
-		sprintf_s(buf, "HumMan Balls: %d %7.1f FPS Frame: %.3f ms  Phys: %.3f  Draw: %.3f ",
+		sprintf_s(buf, "Balls: %d Draw FPS: %7.1f Frame: %.1f ms Phys FPS: %d Phys frame: %.1f ms",
 			balls_count,
-			balu_time.GetFPS(),
-			balu_time.GetTick() * 1000,
-			phys_time * 1000,
-			draw_time * 1000);
+			draw_fps.GetFPS(),
+			draw_fps.GetTick() * 1000,
+			phys_fps_value,
+			phys_time*1000);
 		glfwSetWindowTitle(window, &buf[0]);
+		puts(buf);
 	}
-
 	{
-		render->Clear(1, 1);		
+		render->Clear(1, 1);
 		{
-			while (mtx_trylock(&thread_sync.particles_lock) != thrd_success);
+			draw_waiting = true;
 
-			render->VertexBuffer.Data(pos_buff, balls_count*sizeof(TVec2_Float), &balls_pos[0]);
-			render->VertexBuffer.Data(color_buff, balls_count*sizeof(TVec<unsigned char, 4>), &balls_color[0]); //цвета тоже загружаем повторно, т.к. шары мен€ют своЄ положение в массиве в цел€х оптимизации
-			//TVec<short, 2>* points = (TVec<short, 2>*)render->VertexBuffer.Map(pos_buff, TVBAccess::Write);
-			//SendBallsPos(points);
-			//render->VertexBuffer.Unmap(pos_buff);
+			while (balls_draw_lock.test_and_set(std::memory_order_acquire)) ;
 
-			
-			//render->Blend.Func("dc*(1-sa)+sc*sa");
-			//render->Set.Projection(ortho_m);
+			CopyPosAndColor(&local_balls_pos[0], &local_balls_color[0]);
 
-			// Wait for particle physics thread to be done
-			
-			//mtx_lock(&thread_sync.particles_lock);
-			//while (!glfwWindowShouldClose(window) &&
-			//	thread_sync.p_frame <= thread_sync.d_frame)
-			{
-				struct timespec ts;
-				clock_gettime(CLOCK_REALTIME, &ts);
-				ts.tv_nsec += 100000000;
-				//cnd_timedwait(&thread_sync.p_done, &thread_sync.particles_lock, &ts);
-			}
+			draw_waiting = false;
+
+			balls_draw_lock.clear(std::memory_order_release);		
+
+			render->VertexBuffer.Data(pos_buff, balls_count * sizeof(TVec2_Float), &local_balls_pos[0]);
+#ifdef USE_COLOR
+			render->VertexBuffer.Data(color_buff, balls_count * sizeof(TVec<unsigned char, 4>), &local_balls_color[0]);
+#endif
 
 			render->Draw(streams, TPrimitive::Points, balls_count);
-			//streams.Clear();
-
-			// We are done with the particle data
-			mtx_unlock(&thread_sync.particles_lock);
-			//cnd_signal(&thread_sync.d_done);
 		}
-
-		if (false){
-			//render->Blend.Func("dc*sa+sc*(1-sa)");
-			TQuad<float, 3> q;
-			TQuad<float, 2> t;
-			t.Set(TVec2(0.5, 0.5), 1);
-			q.Set(TVec2(0, 0), TVec2(1, 1), 0);
-			streams.AddStream(TStream::Vertex, TDataType::Float, 3, &q);
-			streams.AddStream(TStream::TexCoord, 0, TDataType::Float, 2, &t);
-			render->Texture.Enable(true);
-			//render->Texture.Bind(tex);
-			render->Set.Color(1, 1, 1, 1);
-			render->Set.Projection(TMatrix<float, 4>::GetIdentity());
-			render->Draw(streams, TPrimitive::Quads, 4);
-			render->Texture.Enable(false);
-			streams.Clear();
-		}
-
 	}
 
-	draw_time = balu_time.TimeDiff(balu_time.GetTime(), t);
+	return true;
 }
-
 static void resize_callback(GLFWwindow* window, int width, int height)
 {
-	glViewport(0, 0, width, height);
-	aspect_ratio = height ? width / (float)height : 1.f;
+	render->Set.Viewport(TVec2i(width, height));
 }
-
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
 	if (action == GLFW_PRESS)
@@ -272,77 +169,75 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
 {
-	cursor_pos = TVec2(xpos, ypos);
+	TVec2i temp = TVec2i(xpos, ypos);
+
+	TVec2 tt = render->WindowToClipSpace(temp[0], temp[1]);
+	TVec3 mm = (ortho_m_inv*(TVec4(TVec3(tt, 0), 1))).GetHomogen();
+	float f = powf(2, fracture_part);
+	SetAttractorPos(TVec2(mm[0] / f, mm[1] / f));
 }
 
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 {
-	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+	if (button == GLFW_MOUSE_BUTTON_LEFT)
 	{
+		SetAction(action == GLFW_PRESS ? 1 : 0);
+	}
+	if (button == GLFW_MOUSE_BUTTON_RIGHT)
+	{
+		SetAction(action == GLFW_PRESS ? -1 : 0);
 	}
 }
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
-	attractor_size += (yoffset)*0.003;
-	Clamp(0.0f, room_size / 2.0f, attractor_size);
-	attractor_size = Clamp(0.01f, 1000.0f, attractor_size);
+	ChangeAttractor(yoffset);
 }
 
 static int physics_thread_main(void* arg)
 {
 	GLFWwindow* window = (GLFWwindow*)arg;
-
 	for (;;)
 	{
-		//if (balu_time.GetDelta()<0.001)return 0;
-		
-
-		while (mtx_trylock(&thread_sync.particles_lock) != thrd_success);
-
-		// Wait for particle drawing to be done
-		//while (!glfwWindowShouldClose(window) &&
-		//	thread_sync.p_frame > thread_sync.d_frame)
-		{
-			struct timespec ts;
-			clock_gettime(CLOCK_REALTIME, &ts);
-			ts.tv_nsec += 100000000;
-			//cnd_timedwait(&thread_sync.d_done, &thread_sync.particles_lock, &ts);
-		}
-
-		if (glfwWindowShouldClose(window))
+		if (should_close)
 			break;
 
-		balu_time.Tick();
+		if (draw_waiting)
+			thrd_yield();
+
+		while (balls_draw_lock.test_and_set(std::memory_order_acquire));
 
 		if (!balls_pause)
 		{
-			UpdateBalls(balu_time, !test_broadphase);
+				UpdateBalls(!test_broadphase);
+				phys_fps.Tick();
 		}
 
-		// Unlock mutex and signal drawing thread
-		mtx_unlock(&thread_sync.particles_lock);
-		//cnd_signal(&thread_sync.p_done);
-	}
+		balls_draw_lock.clear(std::memory_order_release);
 
+		if (phys_fps.ShowFPS())
+		{
+			phys_fps_value = phys_fps.GetFPS();
+			phys_time = phys_fps.GetTick();
+		}
+	}
+	printf("phys cycle stopped\n");
 	return 0;
 }
 
 int main(int argc, char** argv)
 {
+	glfwSetErrorCallback(error_callback);
 	int ch, width, height;
 	thrd_t physics_thread = 0;
 	GLFWwindow* window;
-
 	if (!glfwInit())
 	{
 		fprintf(stderr, "Failed to initialize GLFW\n");
 		exit(EXIT_FAILURE);
 	}
-
 	width = 640;
 	height = 480;
-
 	window = glfwCreateWindow(width, height, "Particle Engine", NULL, NULL);
 	if (!window)
 	{
@@ -350,7 +245,6 @@ int main(int argc, char** argv)
 		glfwTerminate();
 		exit(EXIT_FAILURE);
 	}
-
 	glfwMakeContextCurrent(window);
 
 	glfwSwapInterval(1);
@@ -360,47 +254,40 @@ int main(int argc, char** argv)
 	glfwSetCursorPosCallback(window, cursor_position_callback);
 	glfwSetMouseButtonCallback(window, mouse_button_callback);
 	glfwSetScrollCallback(window, scroll_callback);
-
 	// Set initial aspect ratio
 	glfwGetFramebufferSize(window, &width, &height);
-	resize_callback(window, width, height);
-
+	
 	render = new TBaluRender(TVec2i(width, height));
 
+	resize_callback(window, width, height);
+
 	Init();
-
-	balu_time.Start();
-
-	// Set initial times
-	thread_sync.t = 0.0;
-	thread_sync.dt = 0.001f;
-
-	mtx_init(&thread_sync.particles_lock, mtx_timed);
-	cnd_init(&thread_sync.p_done);
-	cnd_init(&thread_sync.d_done);glfwSetTime(0.0);
-
+	draw_fps.Start();
+	phys_fps.Start();
+	
+	glfwSetTime(0.0);
 	if (thrd_create(&physics_thread, physics_thread_main, window) != thrd_success)
 	{
 		glfwTerminate();
 		exit(EXIT_FAILURE);
 	}
-
 	glfwSetTime(0.0);
-
+	printf("Starting main cycle\n");
 	while (!glfwWindowShouldClose(window))
 	{
-		draw_scene(window, glfwGetTime());
-
-		glfwSwapBuffers(window);
+		if (draw_scene(window, glfwGetTime()))
+		{
+			glfwSwapBuffers(window);			
+		}	
 		glfwPollEvents();
 	}
-
+	should_close = true;
+	printf("Exiting\n");
 	thrd_join(physics_thread, NULL);
-
 	delete render;
-
+	printf("Render deleted\n");
 	glfwDestroyWindow(window);
 	glfwTerminate();
-
-	exit(EXIT_SUCCESS);
+	printf("glfwTerminated\n");
+	return 0;
 }
